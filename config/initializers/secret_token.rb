789384a46f8824b08cec1c251ac2df514805305d
jrufasto5 <jrufasto5@gmail.com>
2012-03-15 21:06:27 -0500

    iniciando repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TdewPc1::Application.config.secret_token = '5a1edfeb25c3d90e13c727ea422ac9633683dee5e4d63878a30921477347ec03a604eb282c3c008a4d9d20e440cc3dd274cc9695c0286571c6728a03cf480928'
